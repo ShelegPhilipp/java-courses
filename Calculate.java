5d6d2af1c2606e3fdf0d16adb917617fa2c4ab07
ShelegPhilipp <shelegfilipp@gmail.com>
2018-02-27 17:47:33 +0300

    User imput

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,25 @@ public class Calculate {
 	
 	public static void main( String[] arg){
 		System.out.println("Calculate...");
+		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1  ]);
+		
+		int sum = first + second;
+		System.out.println("sum = " + sum );
+		
+		int sub = first - second;
+		System.out.println("Sub = " + sub );
+		
+		int mult = first*second;
+		System.out.println("Mult = " + mult );
+		
+		int division = first/second;
+		System.out.println("division = " + division );
+		
+		double power =  Math.pow(first, second);
+		System.out.println("power = " + power );
+		
 	}
 	
 }
